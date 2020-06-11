68411eb12f97ace0dd8d04d37e41e9597fea936b
Ruchi Sandilya <ruchi9451100@gmail.com>
2020-06-11 17:02:27 -0400

    Create eigen.m

---
+++ eigen.m
@@ -0,0 +1,50 @@
+% Matlab code for computing eigenvalues of linearized operator
+
+clear all
+load linear.mat
+load freeinds.txt
+load pinds.txt
+who
+
+format long
+% Set some parameters
+mode = 'min'; % 'min' or 'lqr'
+ne = 20;      % how many eigenvalues to compute
+
+As = [Avs1, Ats2];
+[n1,n2] = size(As);
+A = [Aa,  As; 
+     As', sparse(n2,n2)];
+M = [Ma, sparse(n1,n2); 
+     sparse(n2,n2+n1)];
+
+% number of Lanczos vectors
+opts.p = 50;
+
+% Compute eigenvalues,vectors of (A,M)
+[Vt,D1,flag] = eigs(A,M,ne,'SM',opts);
+assert(flag==0)
+save('eig1.mat','Vt','D1')
+disp('Eigenvalues of A')
+D1=diag(D1)
+
+% find unstable eig
+iu = find(real(D1) > 0);
+nu = length(iu);
+fprintf(1, 'Number of unstable eigenvalues of A = %d\n', nu)
+
+
+% Compute eigenvalues,vectors of (A^T,M^T)
+[Zt,D2,flag] = eigs(A',M',ne,'SM',opts);
+assert(flag==0)
+save('eig2.mat','Zt','D2')
+disp('Eigenvalues of A^T')
+D2=diag(D2)
+
+
+% find unstable eig
+iu = find(real(D2) > 0);
+nu2= length(iu);
+fprintf(1, 'Number of unstable eigenvalues of A^T= %d\n', nu2)
+assert(nu == nu2)
+
