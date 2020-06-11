ef2526b06903eab83f097b17dab05bcf462132d4
Ruchi Sandilya <ruchi9451100@gmail.com>
2020-06-11 16:23:55 -0400

    Create param.py
    Stores parameters

---
+++ param.py
@@ -0,0 +1,8 @@
+# Given parameters of the Navier-Stokes-Boussinesq equations
+udeg = 2 
+Re = 100.0     #Reynolds number
+Gr = Re**2/0.9 #Grashof number
+Pr = 0.7       #Prandtl number
+
+# inlet control zone location: as in Weiwei Hu thesis
+y3,y4 = 0.7,0.9 
